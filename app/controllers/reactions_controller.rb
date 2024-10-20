03d0b47ef402ef9605f991adec15b952192eef6f
Carlos Santana <carlos.santanag@live.cl>
2024-10-20 18:44:00 -0300

    v5.5 - Agrega borde a reacción del usuarioo

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,7 +1,7 @@
 class ReactionsController < ApplicationController
   def new_user_reaction
     @type = params[:reaction_type]
-    @article = Article.find(params[:article_id]) if params[:article_id]
+    @article = Article.find(params[:article_id])  if params[:article_id]
     @comment = Comment.find(params[:comment_id])  if params[:comment_id]
     @kind = params[:kind]
 
@@ -15,14 +15,19 @@ class ReactionsController < ApplicationController
     respond_to do |format|
       (@type == "comment") ? reaction_comment = Reaction.find_by(user_id: @user, comment_id: @comment.id) : reaction_article = Reaction.find_by(user_id: @user.id,  article_id: @article.id)
 
-      if reaction_article || reaction_comment
-        format.html { redirect_to article_path(@article), notice: "Ud. ya reaccionó a este artículo" }
+      if reaction_article
+        format.html { redirect_to article_path(@article), notice: "Ya reaccionó a este artículo" }
+      elsif reaction_comment
+        format.html { redirect_to article_path(@article), notice: "Ya reaccionó a este comentario" }
       else
+        # (@type == "article") ? @reaction = Reaction.new(user_id: @user.id, article_id:  @article.id, reaction_type: @type, kind: @kind) : @reaction = Reaction.new(user_id: @user.id, comment_id: @comment.id, reaction_type: @type, kind: @kind)
 
-        (@type == "article") ? @reaction = Reaction.new(user_id: @user.id, article_id:  @article.id, reaction_type: @type, kind: @kind) : @reaction = Reaction.new(user_id: @user.id, comment_id: @comment.id, reaction_type: @type, kind: @kind)
+        if @type == "article"
+            @reaction = Reaction.new(user_id: @user.id, article_id:  @article.id, reaction_type: @type, kind: @kind)
+        end
 
         if @reaction.save!
-            format.html { redirect_to article_path(@article), notice: "Reacción registrada" }
+          format.html { redirect_to article_path(@article), notice: "Reacción registrada" }
         else
           format.html { redirect_to article_path(@article), notice: "Ha ocurrido algo extraño - No se pudo registrar reacción" }
         end
